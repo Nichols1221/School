0abe3ab8dc3f601fd0a55969218856880398af30
Nichols1221 <nichols1221@gmail.com>
2022-11-21 10:35:58 -0600

    11-21-2022

--- Capstone/axe-handle/src/App.js
+++ Capstone/axe-handle/src/App.js
@@ -2,6 +2,13 @@
 
 function App() {
   return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
+     
+
+    </Routes>
     
   );
 }
